e7327e438bfddc1d5c289bb935e83dc4e15f9e6c
jess-chris <jtq.christensen@gmail.com>
2022-12-31 16:34:59 -0800

    First non repeating character

---
+++ completed/easy/firstNonRepeat.js
@@ -0,0 +1,26 @@
+function firstNonRepeatingCharacter(string) {
+
+  const obj = {};
+
+  for (let x = 0; x < string.length; x++) {
+
+    obj[string[x]] === undefined ? obj[string[x]] = x : obj[string[x]] = -1;
+
+  }
+
+  console.log(obj)
+
+  const values = Object.values(obj).filter(el => el >= 0).sort((a,b) => a - b);
+
+  console.log(values)
+
+  if (values.length !== 0) return values[0];
+
+  return -1;
+}
+
+
+console.log(firstNonRepeatingCharacter("faadabcbbebdf"))
+
+// Do not edit the line below.
+exports.firstNonRepeatingCharacter = firstNonRepeatingCharacter;
